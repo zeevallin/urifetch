5223701cedff90c1ab7bba087338bd5083fbe70a
Philip Vieira <philip@vallin.se>
2012-01-19 16:12:46 +0100

    Pushed to 0.0.4
    Signed-off-by: Philip Vieira <philip@vallin.se>

--- lib/urifetch/version.rb
+++ lib/urifetch/version.rb
@@ -4,8 +4,8 @@ module Urifetch
     
     MAJOR = 0
     MINOR = 0
-    PATCH = 3
-    BUILD = 'rc1'
+    PATCH = 4
+    BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
     
